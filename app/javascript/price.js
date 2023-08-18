d1deb64ed1d7823adbbbf04d04ee5fe154b031fe
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-18 19:36:39 +0900

    販売手数料や販売利益の表示が変わる

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,14 +1,17 @@
 window.addEventListener('load', () => {
 
-const priceInput = document.getElementById("item-price");
-    priceInput.addEventListener("input", () => {
-        const inputValue = priceInput.value;
-        console.log(inputValue);
+  const priceInput = document.getElementById("item-price");
+  const salesFeeDisplay = document.getElementById("add-tax-price");
+  const profitDisplay = document.getElementById("profit");
 
-    })
+ priceInput.addEventListener("input", () => {
+  const price = parseFloat(priceInput.value);
+  const salesFee = Math.floor(price * 0.1) || 0; 
+  const profit = Math.floor(price - salesFee); 
 
-
-
+  salesFeeDisplay.textContent = salesFee ? `${salesFee.toLocaleString()}円` : "";
+  profitDisplay.textContent = profit ? `${profit.toLocaleString()}円` : "";
+});
 
 })
 
