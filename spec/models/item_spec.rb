0db85c7db6cdc18ce224df6fb091202ca521cb86
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-18 22:43:56 +0900

    テストコード-1

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -16,11 +16,10 @@ RSpec.describe Item, type: :model do
             expect(@item).to be_valid
         end
 
-        
       end
       
   
-    context '内容に問題ある場合' do
+      context '内容に問題ある場合' do
 
         it 'ユーザーが紐付いていなければ出品できない' do
              @item.user = nil
@@ -29,10 +28,63 @@ RSpec.describe Item, type: :model do
   
         end
 
-    end
+        it '商品名が空の場合は無効であること' do
+          @item.item_name = ''
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Item name can't be blank")
+        end
+    
+        it '説明が空の場合は無効であること' do
+          @item.description = ''
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Description can't be blank")
+        end
+    
+        it 'カテゴリが空の場合は無効であること' do
+          @item.category = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Category can't be blank")
+        end
+    
+        it '商品状態が空の場合は無効であること' do
+          @item.condition = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Condition can't be blank")
+        end
+    
+        it '配送料が空の場合は無効であること' do
+          @item.shipping_fee = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
+        end
+    
+        it '都道府県が空の場合は無効であること' do
+          @item.prefecture = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+        end
+    
+        it '配送日数が空の場合は無効であること' do
+          @item.shipping_date = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Shipping date can't be blank")
+        end
+    
+        it '価格が空の場合は無効であること' do
+          @item.price = ''
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Price can't be blank")
+        end
+    
+        it '価格が半角数字でないと無効であること' do
+          @item.price = '３０００'
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Price is not a number")
+        end
+    
+
+      end
    
-
-
   end
 
 
