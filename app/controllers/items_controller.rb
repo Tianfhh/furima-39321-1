dedd8ff1bd5e2b423c1a5c9da810fc55bdeb115a
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-18 17:08:31 +0900

    出品ができたがトップページには表示されていない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,6 +12,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    @item.user = current_user
     if @item.save
       redirect_to items_path
     else
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,6 +11,8 @@ class Item < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_fee
