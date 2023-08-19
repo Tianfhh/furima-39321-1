1a2fdb817b4ae0a79adb510b0b4a29e1ce279543
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-19 15:39:50 +0900

    user_idを保存する記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_date_id, :price, :image)
+    params.require(:item).permit(:user, :item_name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_date_id, :price, :image)
   end
 
 end
