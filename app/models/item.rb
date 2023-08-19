d35646d4b84321b83fadfadbea50126d33752aec
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-19 15:43:39 +0900

    1を選択できないよう

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,11 @@ class Item < ApplicationRecord
   
   validates :item_name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_fee_id, presence: true
-  validates :prefecture_id, presence: true  
-  validates :shipping_date_id, presence: true
+  validates :category_id, presence: true, numericality: { greater_than: 1 }
+  validates :condition_id, presence: true, numericality: { greater_than: 1 }
+  validates :shipping_fee_id, presence: true, numericality: { greater_than: 1 }
+  validates :prefecture_id, presence: true  , numericality: { greater_than: 1 }
+  validates :shipping_date_id, presence: true, numericality: { greater_than: 1 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, numericality: { only_integer: true }
 
   belongs_to :user
