2ee00ee9c4de875bc73b3c62769d3d3a9c32bd5d
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-20 15:40:02 +0900

    updateアクションのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
 
   
-  resources :items, only: [:new, :create, :index, :show, :edit]
+  resources :items, only: [:new, :create, :index, :show, :edit, :update]
 
 
  
