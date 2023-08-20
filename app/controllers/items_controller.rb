7f9b53bf9a243d91805084714694f38e52af6213
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-20 19:13:44 +0900

    商品削除機能修正-1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,9 +46,8 @@ class ItemsController < ApplicationController
 
   def destroy
     
-    @item.destroy
-
     if @item.user == current_user
+       @item.destroy
       redirect_to root_path
     else
       redirect_to root_path
