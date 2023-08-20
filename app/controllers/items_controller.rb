6ea1214593aef1cc033ad538f0f73791a0c1b654
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-20 19:06:24 +0900

    ログイン中のユーザーのみ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
