f81d4184280f3f18dde8f3193f115633cc71c4df
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-22 12:34:28 +0900

    エラーメッセージの表示

--- app/models/buyer_shipping_address.rb
+++ app/models/buyer_shipping_address.rb
@@ -4,14 +4,15 @@ class BuyerShippingAddress
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
     validates :city
     validates :house_number 
-    validates :phone_number
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "Phone number is invalid. Input only 10 or 11 digits." }
     validates :user_id
     validates :item_id
   end
 
-  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+  
 
   def save
     # binding.pry
