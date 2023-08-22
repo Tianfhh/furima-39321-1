7e30f0ec6c18ce9cc94c07197ec1dd7aef030fd8
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-22 19:09:40 +0900

    クレジットカードのところ入力できる

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -12,7 +12,7 @@ class BuyersController < ApplicationController
     @item = Item.find(params[:item_id])
     @buyer_shipping_address = BuyerShippingAddress.new(buyer_params)
     if @buyer_shipping_address.valid?
-      # pay_item
+      pay_item
       @buyer_shipping_address.save
       return redirect_to root_path
     else
@@ -23,16 +23,16 @@ class BuyersController < ApplicationController
   private
 
   def buyer_params
-    params.require(:buyer_shipping_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :price).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:buyer_shipping_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token).merge(user_id: current_user.id, item_id: @item.id)
   end
 
-  # def pay_item
-  #   Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  
-  #   Payjp::Charge.create(
-  #     amount: buyer_params[:price],
-  #     card: buyer_params[:token],
-  #     currency: 'jpy'
-  #   )
-  # end
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: @buyer_shipping_address.token,
+      currency: 'jpy'
+    )
+  end
 
 end
--- app/models/buyer_shipping_address.rb
+++ app/models/buyer_shipping_address.rb
@@ -1,6 +1,6 @@
 class BuyerShippingAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :price, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
