183ac59da44b434d0075e324c32e2957326bbe70
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-23 14:59:15 +0900

    テストコード内で紐づけを行っているため削除

--- spec/factories/buyer_shipping_addresses.rb
+++ spec/factories/buyer_shipping_addresses.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :buyer_shipping_address do
+    
     postal_code   { '123-4567' }
     prefecture_id { 1 }
     city          { '東京都' }
     house_number  { '1-2-3' }
     phone_number  { '09012345678' }
-    association :item
-    association :user
+
   end
 end
