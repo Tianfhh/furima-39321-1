f250055e904245cd04e998b9c646d8d6b668ad1f
Tianfhh <131650142+Tianfhh@users.noreply.github.com>
2023-08-23 17:46:02 +0900

    Basic認証導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-
+  before_action :basic_auth
 
   
 
@@ -12,4 +12,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date ])
   end
   
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
+  end
+
+
 end
